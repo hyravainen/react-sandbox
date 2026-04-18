314c508b1fd462e42e5806027a1a2ea8a57c2ff3
Juho <e2402268@edu.vamk.fi>
2026-04-18 10:20:24 +0300

    Header added

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header(){
+return(
+    <header>
+        <h1 className="bg-orange-500 text-white font-bold flex justify-between items-center sticky top-0 z-50">
+            <span>TechBurger</span>
+            <span>Shopping cart</span>
+        </h1>
+    </header>
+)
+}
