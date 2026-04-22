0e8b51a5425c49af2d6a91413712bf2b09615eaa
Emppu K <emmilotta.kallio@gmail.com>
2026-04-22 19:03:06 +0300

    1.6 integration done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Header from './components/Header';
+import ProductCard from './components/ProductCard';
 import Footer from './components/Footer';
 
 function App() {
@@ -6,8 +7,12 @@ function App() {
 
   return (
     <>
+    <div className="bg-gray-50 min-h-screen flex flex-col">
     <Header></Header>
       <h1 className="text-center bg-red-400 text-4xl py-4">Template for the TechBurger project</h1>
+      <ProductCard></ProductCard>
+      <Footer></Footer>
+      </div>
     </>
   )
 }
